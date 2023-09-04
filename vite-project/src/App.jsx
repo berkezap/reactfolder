30bec4e7d9c4b5d8c2b0ef3507255f028f06bc1e
berkezap <ihsanberke.ozsap@std.yeditepe.edu.tr>
2023-09-05 01:47:15 +0300

    third commit
    className

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,19 +4,24 @@ function App() {
   const students = 43000;
   const nameSurname = "Berke Ozsap";
   const dogruMu = true;
+  const url = "https://picsum.photos/250/250"
 
   return (
-    <div>
-      <h1>İsim Soyisim : {nameSurname} </h1>
-      <p>Öğrenci Sayisi: {students}</p>
+    // <div>
+    //   <h1>İsim Soyisim : {nameSurname} </h1>
+    //   <p>Öğrenci Sayisi: {students}</p>
       
 
-      {/* ? den sonra gelen true olduğunda, : dan sonra gelen false olduğunda çalışır */}
-      {dogruMu ? <p>Öğrenci Sayisi </p> :  <h1>İsim Soyisim :</h1>}
+    //   {/* ? den sonra gelen true olduğunda, : dan sonra gelen false olduğunda çalışır */}
+    //   {dogruMu ? <p>Öğrenci Sayisi </p> :  <h1>İsim Soyisim :</h1>}
 
-      <p>deneme</p>
-      <input type="text" />
-      <input type="date" />
+    //   <p>deneme</p>
+    //   <input type="text" />
+    //   <input type="date" />
+    //   <img src={url} alt="" /> 
+    // </div>
+
+    <div className='box'>
     </div>
   );
 }
