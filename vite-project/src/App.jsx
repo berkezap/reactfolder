499786082e9e0f7497fd6d20938e8219a619760a
berkezap <ihsanberke.ozsap@std.yeditepe.edu.tr>
2023-09-04 17:14:05 +0300

    first commit
    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+  const students = 43000;
+  const nameSurname = "Berke Ozsap";
+  const dogruMu = true;
+
+  return (
+    <div>
+      <h1>İsim Soyisim : {nameSurname} </h1>
+      <p>Öğrenci Sayisi: {students}</p>
+      
+
+      {/* ? den sonra gelen true olduğunda, : dan sonra gelen false olduğunda çalışır */}
+      {dogruMu ? <p>Öğrenci Sayisi </p> :  <h1>İsim Soyisim :</h1>}
+
+
+      <input type="text" />
+      <input type="date" />
+    </div>
+  );
+}
+
+export default App
+
